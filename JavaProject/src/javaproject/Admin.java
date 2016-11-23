2943f4f133edb3f00422df6a338a05a88273359e
Matt <matthew.bradley@ryerson.ca>
2016-11-23 01:57:06 -0500

    put together a pattern for how to ensure ommands are parsed reliably. code has been marked off. will continue in morning

--- JavaProject/src/javaproject/Admin.java
+++ JavaProject/src/javaproject/Admin.java
@@ -19,40 +19,64 @@ public class Admin extends User{
     public static Admin getInstance(){
         return oneTimeInstance;
     }
+    
+    /*
+    steps in checking each token level
+    
+    does the token exist?
+    is it a valid variable? (ie non-empty, must be checked after we're sure it exists)
+    is it a valid in the context of it's role?
+    */
 
+    //******************need to check if object already exists when creating
     @Override
     public void newCommand(String line) {
         String[] tokens = line.split(" ");
         if("create".equals(tokens[0].toLowerCase())) {
-            if(tokens.length >= 2) {
-                if("student".equals(tokens[1].toLowerCase())) { //******************need to check if object already exists
-                    if(tokens.length == 4 && tokens[2].length() > 0 && tokens[3].length() > 0) {
-                        User temp = new Student(tokens[2], tokens[3]);
-                    } else {
+            if(tokens.length < 2) {
+                System.out.println("You need to specify something to create.");
+            } else {
+                if("student".equals(tokens[1].toLowerCase())) {
+                    
+                    
+                    
+                    /*
+                    if(true) {
                         System.out.println("Insufficient or invalid parameters supplied.");
                         System.out.println("Usage: create student <username> <password>");
-                    }
-                } else if("professor".equals(tokens[1].toLowerCase())) {
-                    if(tokens.length == 4 && tokens[2].length() > 0 && tokens[3].length() > 0) {
-                        JavaProject.users.add(new Professor(tokens[2], tokens[3]));
                     } else {
+                        User temp = new Student(tokens[2], tokens[3]);
+                    }
+                    */
+                } else if("professor".equals(tokens[1].toLowerCase())) {
+                    
+                    
+                    
+                    /*
+                    if(true) {
                         System.out.println("Insufficient or invalid parameters supplied.");
                         System.out.println("Usage: create professor <username> <password>");
-                    }
-                } else if("course".equals(tokens[1].toLowerCase())) {
-                    if(tokens.length == 3 && tokens[2].length() > 0) {
-                        JavaProject.courses.add(new Course(tokens[2]));
                     } else {
+                        JavaProject.users.add(new Professor(tokens[2], tokens[3]));
+                    }
+                    */
+                } else if("course".equals(tokens[1].toLowerCase())) {
+                    
+                    
+                    
+                    /*
+                    if(true) {
                         System.out.println("Insufficient or invalid parameters supplied.");
                         System.out.println("Usage: create course <name>");
+                    } else {
+                        JavaProject.courses.add(new Course(tokens[2]));
                     }
+                    */
                 } else {
                     System.out.println("Unknown type \"" + tokens[1] +"\".");
                 }
-            } else {
-                System.out.println("You need to specify something to create.");
             }
-        } else if("command2".equals(tokens[0].toLowerCase())) {
+        } else if("dummyCommand".equals(tokens[0].toLowerCase())) {
             
         } else {
             System.out.println("Unknown Command \"" + line + "\". Try \"help\"");
